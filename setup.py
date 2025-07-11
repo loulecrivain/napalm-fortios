3f775f3cdb81c84aa408d1ddb590722f80b7b8c3
lou lecrivain <lou.lecrivain@wdz.de>
2025-07-11 11:18:23 +0200

    switch deps to setup.py and bump ncclient to closest compatible version

--- setup.py
+++ setup.py
@@ -3,17 +3,8 @@
 import uuid
 
 from setuptools import setup, find_packages
-try:
-    from pip._internal.req import parse_requirements
-except ImportError:
-    from pip.req import parse_requirements
-__author__ = 'David Barroso <dbarrosop@dravetech.com>'
 
-install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
-try:
-    reqs = [str(ir.req) for ir in install_reqs]
-except:
-    reqs = [str(ir.requirement) for ir in install_reqs]
+__author__ = 'David Barroso <dbarrosop@dravetech.com>'
 
 setup(
     name="napalm-fortios",
@@ -30,5 +21,10 @@ setup(
     ],
     url="https://github.com/napalm-automation/napalm-fortios",
     include_package_data=True,
-    install_requires=reqs,
+    install_requires=[
+        'napalm',
+        'future',
+        'ncclient==0.6.15',
+        'pyfg @ git+https://github.com/xXKnightRiderXx/pyfg.git#egg=pyfg',
+    ],
 )
